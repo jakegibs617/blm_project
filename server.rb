0c0649a4751f6e09fcfccd9ed49376acd43f4131
Jacob <jakegibs617@gmail.com>
2015-09-07 13:59:19 -0400

    added keys and config

--- server.rb
+++ server.rb
@@ -3,6 +3,10 @@ require 'sinatra/json'
 require 'httparty'
 require 'pry'
 
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key    = ENV['YOUR_CONSUMER_KEY']
+  config.consumer_secret = ENV['YOUR_CONSUMER_SECRET']
+end
 # get '/api/v1/tags' do
 #   response = HTTParty.get('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
 #   tags = Hash.new 0
